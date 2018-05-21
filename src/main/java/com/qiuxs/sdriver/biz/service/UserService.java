a44ca36b3435f153b1144f2442bf73760e1bb003
qiuxs <1162796928@qq.com>
2018-05-21 20:51:10 +0800

    删除无用文件

--- src/main/java/com/qiuxs/sdriver/biz/service/UserService.java
+++ src/main/java/com/qiuxs/sdriver/biz/service/UserService.java
@@ -1,9 +1,12 @@
 package com.qiuxs.sdriver.biz.service;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.qiuxs.cuteframework.core.persistent.modal.PropertyWrapper;
 import com.qiuxs.cuteframework.core.persistent.service.AbstractDataService;
 import com.qiuxs.sdriver.biz.dao.UserDao;
 import com.qiuxs.sdriver.biz.entity.User;
@@ -23,5 +26,10 @@ public class UserService extends AbstractDataService<Long, User, UserDao> {
 	@Override
 	protected UserDao getDao() {
 		return this.userDao;
+	}
+
+	@Override
+	protected void initProps(List<PropertyWrapper<?>> props) {
+		
 	}
 }
