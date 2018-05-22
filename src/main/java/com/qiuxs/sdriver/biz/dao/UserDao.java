fa5d6e09d9052ab4fb1dc8c689150dedcfa3246e
qiuxs <1162796928@qq.com>
2018-05-22 20:47:04 +0800

    自动生成提到框架层

--- src/main/java/com/qiuxs/sdriver/biz/dao/UserDao.java
+++ src/main/java/com/qiuxs/sdriver/biz/dao/UserDao.java
@@ -1,14 +1,11 @@
 package com.qiuxs.sdriver.biz.dao;
 
-import org.apache.ibatis.annotations.SelectProvider;
 import org.springframework.stereotype.Repository;
 
+import com.qiuxs.cuteframework.core.persistent.dao.IBaseDao;
 import com.qiuxs.sdriver.biz.entity.User;
 
 @Repository
-public interface UserDao {
-
-	@SelectProvider(type = DynamicSqlProvider.class, method = "dynamicSQL")
-	public User get(Long id);
+public interface UserDao extends IBaseDao<Long, User> {
 
 }
--- src/main/java/com/qiuxs/sdriver/biz/entity/User.java
+++ src/main/java/com/qiuxs/sdriver/biz/entity/User.java
@@ -1,17 +1,13 @@
 package com.qiuxs.sdriver.biz.entity;
 
-public class User {
-	private Long id;
+import com.qiuxs.cuteframework.core.persistent.entity.impl.AbstractEntity;
+
+public class User extends AbstractEntity<Long> {
+	
+	private static final long serialVersionUID = -7299851880162666341L;
+	
 	private String name;
 	private String password;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
